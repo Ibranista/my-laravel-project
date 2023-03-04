8a37bdcaff4a80a899ccd26fce3e1a8095d371b8
Ibranista <ibranista88@gmail.com>
2023-03-04 10:13:37 +0100

    add about page.

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,9 +3,18 @@
 @section('content-from-base')
     <h1>About</h1>
     <p class="text-blue-600 p-5 px-72">
-        {{-- write a paragraph about mecca --}}
+        we at addisway technologies are a group of young and talented 
+        software developers who are passionate about building
+        web applications and mobile applications.
 
-        Mecca is a city in the Hejaz region of Saudi Arabia that is also capital of Makkah Province. The city is located 70 km (43 mi) inland from Jeddah in a narrow valley at a height of 277 m (909 ft) above sea level, 340 km (210 mi) from Medina and 390 km (240 mi) from Riyadh. Its resident population in 2012 was roughly 2 million, although visitors more than triple this number every year during the hajj period held in the twelfth Muslim lunar month of Dhu al-Hijjah. Mecca is home to the Kaaba, by majority description Islams holiest site, and is the direction of Muslim prayer. Mecca was long ruled by Muhammads descendants, the sharifs, acting either as independent rulers or as vassals to larger polities. It was conquered by Ibn Saud in 1925, during his campaign to unify the Arabian Peninsula into a single state.
+        we are a team of 5 members and we are located in Addis Ababa, Ethiopia.
+
+        we are currently working on a project called Branc which is a
+        web application that helps people to find the best place to live in
+        Addis Ababa, Ethiopia.
+
+
+       
 
     </p>
 @endsection
