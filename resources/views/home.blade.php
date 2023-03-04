1cc403be5dd0c0021f12eb9a10d0b67b2ec04c3a
Ibranista <ibranista88@gmail.com>
2023-03-04 09:54:31 +0100

    add directive for nav and section part.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,30 +1,8 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.base')
 
-        <title>Hello World</title>
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-</head>
-<body>
-<h4>Hello 
-    <?php echo($name); ?>
-    <?php echo('hello')?>
-    {{$name}}
-        <?php if($name == 'Ibrahim'): ?>
-            <h1>Hi Ibrahim</h1>
-        <?php else: ?>
-            <h1>Hi Guest</h1>
-        <?php endif; ?>
-        @if($name == 'Ibrahim')
-            <h1>Hi Ibrahim</h1>
-        @else
-            <h1>Hi Guest</h1>
-        @endif
-</h4>
-</body>
-</html>
+@section('content-from-base')
+    <h1>Home</h1>
+    <p class="text-blue-600">
+        I am a paragraph.
+    </p>
+@endsection
---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Document</title>
+</head>
+<body>
+    <nav class="bg-gray-100 font-bold flex justify-between p-5">
+        <span>Branc</span>
+        <span>Hello, {{$name}}</span>
+    </nav>
+    <section 
+    class="bg-gray-100 h-1/2 flex flex-col justify-center items-center font-bold"
+    >
+        @yield('content-from-base')
+    </section>
+</body>
+</html>
